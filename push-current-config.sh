8c71daf3062bf680c3ffa27d1a198bf8fb51354f
kadmos33 <roman225785@gmail.com>
2021-05-29 11:58:58 +0300

    updated

--- push-current-config.sh
+++ push-current-config.sh
@@ -7,6 +7,9 @@ cp -r ~/.config/bspwm/* ./bspwm
 cp -r ~/.config/sxhkd/* ./sxhkd
 cp -r ~/.config/polybar/* ./polybar
 cp ~/.oh-my-zsh/themes/superjarin.zsh-theme ./zsh
+cp ~/.cshrc ./tcsh
+
+
 
 # Git
 git add .
